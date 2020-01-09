918de676395ae7c45b2507632afc54ab86f64c80
himina <himina1227@gmail.com>
2020-01-09 20:42:38 +0900

    8장 서버배포 Test

--- src/test/java/com/nakcompany/admin/springboot/web/HelloControllerTest.java
+++ src/test/java/com/nakcompany/admin/springboot/web/HelloControllerTest.java
@@ -31,7 +31,7 @@ public class HelloControllerTest {
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
-
+        System.out.printf("aaa");
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
