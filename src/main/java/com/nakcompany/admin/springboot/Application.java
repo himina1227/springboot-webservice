0af2f01622aa325e5124ad7f30e7adbbbba62ff1
himina <himina1227@gmail.com>
2020-01-09 20:07:24 +0900

    8장 서버배포

--- src/main/java/com/nakcompany/admin/springboot/Application.java
+++ src/main/java/com/nakcompany/admin/springboot/Application.java
@@ -3,7 +3,9 @@ package com.nakcompany.admin.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing  //  JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 
